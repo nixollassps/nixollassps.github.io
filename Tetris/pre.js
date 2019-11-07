70cb36100d440794f0d3a90be8392fa1d9f9d813
nixollassps <48063997+nixollassps@users.noreply.github.com>
2019-11-07 13:42:39 -0300

    Jogo tetris

---
+++ Tetris/pre.js
@@ -0,0 +1,13 @@
+function load(){
+loadImage('imagens/a.png');
+   loadImage('imagens/b1.png');
+   loadImage('imagens/b2.png');
+   loadImage('imagens/c.png');
+   loadImage('imagens/d.png');
+   loadImage('imagens/e.png');
+   loadImage('imagens/f.png');
+   loadImage('imagens/g.png');
+   loadImage('imagens/i.png');
+   loadImage('imagens/j.png');
+   loadImage('imagens/k.png');
+}
